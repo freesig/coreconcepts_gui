009a4460370dae6540c78c99dd88f790bff7e971
Tom Gowan <s3549186@student.rmit.edu.au>
2019-09-03 15:26:08 +1000

    hello me

---
+++ hello.js
@@ -0,0 +1,28 @@
+function hello() {
+  holochainclient.connect({ url: "ws://localhost:8080" }).then(({callZome, close}) => {
+    callZome('test-instance', 'hello', 'hello_holo')({"args": {} }).then((result) => update_element(result, 'output'))
+  })
+}
+function update_element(result, id) {
+  var el = document.getElementById(id);
+  var output = JSON.parse(result);
+  el.textContent = " " + output.Ok;
+}
+function update_person(result) {
+  var person = document.getElementById('entry_output');
+  var output = JSON.parse(result);
+  var output = JSON.parse(output.Ok.App[1]);
+  person.textContent = " " + output.name;
+}
+function create_person() {
+  var name = document.getElementById('name').value;
+  holochainclient.connect({ url: "ws://localhost:8080" }).then(({callZome, close}) => {
+    callZome('test-instance', 'hello', 'create_person')({person: {name: name} }).then((result) => update_element(result, 'address_output'))
+  })
+}
+function retrieve_person() {
+  var address = document.getElementById('address_in').value;
+  holochainclient.connect({ url: "ws://localhost:8080" }).then(({callZome, close}) => {
+    callZome('test-instance', 'hello', 'retrieve_person')({address: address}).then((result) => update_person(result))
+  })
+}
