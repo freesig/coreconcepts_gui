9801230c80a8328f0ee669206c6265e512fb7419
Tom Gowan <s3549186@student.rmit.edu.au>
2019-09-05 14:22:36 +1000

    code done for this

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
 function hello() {
-  var port = document.getElementById('port').value;
   var instance = document.getElementById('instance').value;
-  holochainclient.connect({ url: "ws://localhost:" + port }).then(({callZome, close}) => {
+  holochainclient.connect({ url: "ws://localhost:3401"}).then(({callZome, close}) => {
     callZome(instance, 'hello', 'hello_holo')({"args": {} }).then((result) => update_element(result, 'output'))
   })
 }
@@ -18,17 +17,15 @@ function update_person(result) {
 }
 function create_person() {
   var name = document.getElementById('name').value;
-  var port = document.getElementById('port').value;
   var instance = document.getElementById('instance').value;
-  holochainclient.connect({ url: "ws://localhost:" + port }).then(({callZome, close}) => {
+  holochainclient.connect({ url: "ws://localhost:3401"}).then(({callZome, close}) => {
     callZome(instance, 'hello', 'create_person')({person: {name: name} }).then((result) => update_element(result, 'address_output'))
   })
 }
 function retrieve_person() {
   var address = document.getElementById('address_in').value;
-  var port = document.getElementById('port').value;
   var instance = document.getElementById('instance').value;
-  holochainclient.connect({ url: "ws://localhost:" + port }).then(({callZome, close}) => {
+  holochainclient.connect({ url: "ws://localhost:3401"}).then(({callZome, close}) => {
     callZome(instance, 'hello', 'retrieve_person')({address: address}).then((result) => update_person(result))
   })
 }
