992e9b103d6b796729e949bc1f41907f1daf56c1
Tom Gowan <s3549186@student.rmit.edu.au>
2019-09-16 09:52:15 +1000

    const

--- hello.js
+++ hello.js
@@ -17,23 +17,23 @@ function update_person(result) {
 // Zome calls
 
 function hello() {
-  var instance = document.getElementById('instance').value;
+  const instance = document.getElementById('instance').value;
   holochain_connection.then(({callZome, close}) => {
     callZome(instance, 'hello', 'hello_holo')({"args": {} }).then((result) => update_element(result, 'output'))
   })
 }
 
 function create_person() {
-  var name = document.getElementById('name').value;
-  var instance = document.getElementById('instance').value;
+  const instance = document.getElementById('instance').value;
+  const name = document.getElementById('name').value;
   holochainclient.connect({ url: "ws://localhost:3401"}).then(({callZome, close}) => {
     callZome(instance, 'hello', 'create_person')({person: {name: name} }).then((result) => update_element(result, 'address_output'))
   })
 }
 
 function retrieve_person() {
+  const instance = document.getElementById('instance').value;
   var address = document.getElementById('address_in').value;
-  var instance = document.getElementById('instance').value;
   holochainclient.connect({ url: "ws://localhost:3401"}).then(({callZome, close}) => {
     callZome(instance, 'hello', 'retrieve_person')({address: address}).then((result) => update_person(result))
   })
