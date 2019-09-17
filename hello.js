0f421003d92c417f3370f0ce32ec4f352d084134
Tom Gowan <s3549186@student.rmit.edu.au>
2019-09-17 13:23:03 +1000

    clean up

--- hello.js
+++ hello.js
@@ -18,14 +18,16 @@ function update_person(result) {
 
 function hello() {
   holochain_connection.then(({callZome, close}) => {
-    callZome('test-instance', 'hello', 'hello_holo')({"args": {} }).then((result) => update_element(result, 'output'))
+    callZome('test-instance', 'hello', 'hello_holo')({"args": {} })
+      .then((result) => update_element(result, 'output'))
   })
 }
 
 function create_person() {
   var name = document.getElementById('name').value;
   holochain_connection.then(({callZome, close}) => {
-    callZome('test-instance', 'hello', 'create_person')({person: {name: name} }).then((result) => update_element(result, 'address_output'))
+    callZome('test-instance', 'hello', 'create_person')({person: {name: name} })
+      .then((result) => update_element(result, 'address_output'))
   })
 }
 
